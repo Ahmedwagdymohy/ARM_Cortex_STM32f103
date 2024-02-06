47a32a5a9aff87674652b71b362d447a940183f5
ahmed <ahmedwagdymohy@gmail.com>
2024-02-06 02:29:23 +0200

    defines

--- COTS/LIB/STD_TYPES.h
+++ COTS/LIB/STD_TYPES.h
@@ -1,1 +1,33 @@
 /**************************************************************/
+/**********       Author   :  Ahmed_Wagdy      ****************/
+/**********       DATE     :  6 FEB 2024       ****************/
+/**********       file   :  STD_TYPES.H        ****************/
+/**************************************************************/
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char       u8
+typedef unsigned int        u32
+typedef unsigned short      u16
+typedef unsigned long       u64
+
+typedef signed char         s8
+typedef signed int          s32
+typedef signed short        s16
+typedef signed long         s64
+
+typedef float               f32
+typedef double              f64
+
+
+
+
+
+
+
+
+
+#endif  /*STD_TYPES_H_*/
+
+
